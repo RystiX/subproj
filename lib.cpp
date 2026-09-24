3c6ca13bb7fde6440dc64a8f4e009ff84ad8f1be
RystiX <grabruslan.RG@gmail.com>
2026-09-24 09:27:54 +0300

    Добавлена new_feature (detached HEAD)

--- lib.cpp
+++ lib.cpp
@@ -1,5 +1,9 @@
-include "lib.h"
+#include "lib.h"
 
 void hello_from_submodule() {
     std::cout << "Hello from submodule!" << std::endl;
+}
+
+void new_feature() {
+    std::cout << "New feature in submodule" << std::endl;
 }
